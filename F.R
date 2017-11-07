0ddbbd6aded5992cc6153cfe34ed09d724130483
Øyvind Auestad <oyvind.auestad@hotmail.com>
2017-11-07 19:31:45 +0100

    ferdig

--- F.R
+++ F.R
@@ -1,16 +1,18 @@
 library("ggplot2")
+colors = c("#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7", 
+           "#673770", "#D3D93E")
 
-TIMES = 100
-size = TIMES + 3
 sigma = 1
 phi = 3 / 10
 
 data = c(0.58, -1.34, 0.61)
 points = c(11.2, 51.8, 81.4)
 
-t = 1:TIMES
+t = 1:100
+# for task 2
 t = c(t, points)
 
+size = length(t)
 H = abs( matrix(1, size, 1) %*% t - t %*% matrix(1, 1, size) )
 S = sigma * sigma * exp( -phi * H )
 
@@ -26,15 +28,41 @@ S_b = S[(size - 2):size,(size - 2):size]
 my = S_ab %*% solve(S_b, data)
 var = S_a - S_ab %*% solve(S_b) %*% S_ba
 
+simulate_independent = function() {
+  p = ggplot()
+  for (i in 1:10) {
+    # 
+    z = rnorm(100)
+    x = L %*% z
+    df = data.frame(t, x)
+    p = p + geom_line(data = df, aes(t, x), color = colors[i], size = 1)
+  }
+  p = p + theme(text = element_text(size = 20))
+  p = p + labs(title = expression("10 realizations of x(t), with exponential correlation,"~phi[E]~"="~1 / 10), x = "t", y = "x", size = 10)
+  print(p)
+}
+simulate_dependent = function() {
+  t = t[1:100]
+  p = ggplot()
+  for (i in 1:10) {
+    z = rnorm(100)
+    x = my + var %*% z
+    df = data.frame(t, x)
+    p = p + geom_line(data = df, aes(t, x), color = colors[i], size = 1)
+  }
+  df = data.frame(points, data)
+  p = p + geom_point(data = df, aes(points, data), size = 3, color = "black")
+  p = p + theme(text = element_text(size = 20))
+  p = p + labs(title = expression("10 realizations of x(t), given initial data,"~phi[E]~"="~3 / 10), x = "t", y = "x", size = 10)
+  print(p)
+}
+
 main = function() {
+  # task 1
+  #simulate_independent()
   
-  # drawing 100 simulations
-  z = rnorm(TIMES, 0, 1)
-  x = my + var %*% z
-  t = t[1:TIMES]
-  df = data.frame(t, x)
-  p = ggplot() + geom_point(data = df, aes(t, x), color = "blue")
-  print(p + labs(title = "title", x = "x", y = "y"))
+  # task 2
+  simulate_dependent()
 }
 
 main()
--- G.R
+++ G.R
@@ -1,5 +1,6 @@
 library("ggplot2")
 
+
 sigma = 4
 phi = 0.2
 
@@ -32,29 +33,42 @@ my = my_a + S_ab %*% solve(S_b, data - my_b)
 var = S_a - S_ab %*% solve(S_b) %*% S_ba
 var = diag(var)
 
+
+
 main = function() {
   
-  #task 1
+  #task 1, 2, 3
+  p = ggplot()
   t = t[1:141]
   # lower and upper values for 90% prediction interval
   x_lower = my - sqrt(var) * 1.645
   x_upper = my + sqrt(var) * 1.645
+  
+  #adding the expected values and the prediction interval
   df1 = data.frame(t, my)
   df2 = data.frame(t, x_lower)
   df3 = data.frame(t, x_upper)
-  p = ggplot() + geom_line(data = df1, aes(t, my), color = "black", size = 1) +
-    geom_line(data = df2, aes(t, x_lower), color = "blue", size = 0.5) +
-    geom_line(data = df3, aes(t, x_upper), color = "blue", size = 0.5)
-  print(p + labs(title = expression("title," ~ phi[E] ~ "=" ~ 3 / 10), x = "x", y = "y"))
+  p = p + geom_line(data = df1, aes(t, my), color = "darkorange", size = 1) +
+    geom_line(data = df2, aes(t, x_lower), color = "#3399CC", size = 0.5) +
+    geom_line(data = df3, aes(t, x_upper), color = "#3399CC", size = 0.5)
+  #adding the initial data points
+  df4 = data.frame(points, data)
+  p = p + geom_point(data = df4, aes(points, data), size = 3, color = "black")
+  p = p + theme(text = element_text(size = 20))
+  p = p + labs(title = expression("E(x(t)) and the 90% prediction interval, given new data"), x = "t", y = "E(x)")
+  # p = p + geom_line(data = data.frame(c(10, 80), c(57, 57)), aes(c(10, 80), c(57, 57)), color = "grey" )
+  #print(p)
   
-  # task 2, 3
   # the probabilities that x(t) > 57,
   # are gaussian distributed with mean 'my' and variance 'var'
+  q = ggplot()
   z = (57 - my) / sqrt(var)
   p_quality = 1 - pnorm(z)
   df4 = data.frame(t, p_quality)
-  q = ggplot() + geom_line(data = df4, aes(t, p_quality), color = "black", size = 1)
-  #print(q + labs(title = "title", x = "x", y = "y"))
+  q = q + geom_line(data = df4, aes(t, p_quality), color = "darkorange", size = 1)
+  q = q + theme(text = element_text(size = 20))
+  q = q + labs(title = "Probabilites of quality better than 57, given new data", x = "t", y = "p(x(t) > 57)")
+  print(q)
   
 }
 
